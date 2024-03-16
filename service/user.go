ec2a42d648becb579a6c88eb63ef7381e286c643
Qinfeng Wu <qinfengwu90@gmail.com>
2024-03-15 20:52:42 -0600

    format ticket status for email

--- service/user.go
+++ service/user.go
@@ -10,7 +10,7 @@ import (
 func SendEmailUpdate(ticketID int64, newStatus, adminResponse null.String) error {
 	emailContent := fmt.Sprintf("Your ticket #%d has been updated to ", ticketID)
 	if newStatus.Valid {
-		emailContent += fmt.Sprintf("status: %s", newStatus.String)
+		emailContent += fmt.Sprintf("status: %s", formatStatus(newStatus.String))
 	}
 	if adminResponse.Valid {
 		emailContent += fmt.Sprintf(", with admin response: %s", adminResponse.String)
@@ -28,4 +28,17 @@ func GetAllTicketsAndEmailUpdatesFromUser(email, lastName string) ([]model.Ticke
 		return nil, nil, err
 	}
 	return tickets, notifications, nil
+}
+
+func formatStatus(status string) string {
+	switch status {
+	case "new":
+		return "New"
+	case "in_progress":
+		return "In Progress"
+	case "resolved":
+		return "Resolved"
+	default:
+		return "Unknown"
+	}
 }
